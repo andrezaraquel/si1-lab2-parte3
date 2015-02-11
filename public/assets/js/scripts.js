709cf0261ea835c7809451dc42a804b53855ec6b
Abner M. C. Araujo <abner.math.c@gmail.com>
2015-02-11 19:04:26 -0300

    Consertado bug em recomendacao

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
 			success: function(data) {
 				serie = JSON.parse(data);
 				console.log(serie);
-				$("#temporadas").html("");
+				$("#temporadas" + serie.id).html("");
 				for (var i = 0; i < serie.temporadas.length; i++) {
 					var temporada = serie.temporadas[i];
-					var html = "<a class='btn btn-primary btn-xs btn-temporada' href='#serie-" + serie.id + "-t-" + (i + 1) + "' onclick='$(\".temporada\").hide(); $(\"#serie-" + serie.id + "-t-" + (i + 1) + "\").toggle(500);' title='Ver episódios da temporada " + (i + 1) + "'>" + (i + 1) + "</a> Próximo episódio: <span>" + temporada + "</span><br />"
-					$("#temporadas").append(html);
+					var html = "<a class='btn btn-primary btn-xs btn-temporada' href='#serie-" + serie.id + "-t-" + temporada.temporada + "' onclick='$(\".temporada\").hide(); $(\"#serie-" + serie.id + "-t-" + temporada.temporada + "\").toggle(500);' title='Ver episódios da temporada " + temporada.temporada + "'>" + temporada.temporada + "</a> Próximo episódio: <span>" + temporada.nome + "</span><br />"
+					$("#temporadas" + serie.id).append(html);
 				}
 			},
 			fail: function(data) {
